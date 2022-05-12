1c487447add5d4b76b07c333edb01421c2d39b11
teosaputra <teosaputra555@gmail.com>
2022-05-13 05:48:34 +0700

    menambahkan form login dan register di views beserta scripnya di utils

---
+++ utils/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/wpu", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+//menambah 1 data
+// const contact1 = new contact({
+//   nama: "Doddy",
+//   nohp: "081345678",
+//   email: "doddy@email.com",
+// });
+
+// //simpan ke collection
+// contact1.save().then((contact) => console.log(contact));
